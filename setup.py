5566b10734cc231ef24e53761fb09f5bad86796b
Valdecy Pereira <valdecypereira@yahoo.com.br>
2025-02-13 11:41:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='ec_topsis',
-    version='1.0.2',
+    version='1.0.3',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
